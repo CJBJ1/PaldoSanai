0318889ac2b2e0b0343a9e9bcf8ad7c73894c1fe
CJBJ1 <cbjx17@naver.com>
2019-11-16 22:43:08 +0900

    위도경도 클래스 생성

---
+++ app/src/main/java/military/discount/info/GeoLocation.java
@@ -0,0 +1,20 @@
+package military.discount.info;
+
+public class GeoLocation {
+
+    double latitude;
+    double longitude;
+
+    public GeoLocation(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
