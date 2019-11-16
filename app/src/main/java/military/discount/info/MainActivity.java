eacfaba2f9db9638ec40b6f1296f43284d1aafc4
CJBJ1 <cbjx17@naver.com>
2019-11-16 22:15:01 +0900

    구글맵 및 검색창 설정

---
+++ app/src/main/java/military/discount/info/MainActivity.java
@@ -0,0 +1,163 @@
+package military.discount.info;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import military.discount.info.ui.gallery.GalleryFragment;
+import military.discount.info.ui.home.HomeFragment;
+import military.discount.info.ui.send.SendFragment;
+import military.discount.info.ui.share.ShareFragment;
+import military.discount.info.ui.slideshow.SlideshowFragment;
+import military.discount.info.ui.tools.ToolsFragment;
+
+import android.view.Menu;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.bringToFront();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        String title = "My Location";
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        if (id == R.id.nav_home) {
+            navController.navigate(R.id.nav_home);
+        } else if (id == R.id.nav_gallery) {
+            navController.navigate(R.id.nav_gallery);
+        } else if (id == R.id.nav_slideshow) {
+            navController.navigate(R.id.nav_slideshow);
+        } else if (id == R.id.nav_tools) {
+            navController.navigate(R.id.nav_tools);
+        } else if (id == R.id.nav_share) {
+            navController.navigate(R.id.nav_share);
+        } else if (id == R.id.nav_send) {
+            navController.navigate(R.id.nav_send);
+        }
+
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    /*public void setUpFragmentArray(){
+        Fragment homeFragment = new HomeFragment();
+        Fragment galleryFragment = new GalleryFragment();
+        Fragment slideshowFragment = new SlideshowFragment();
+        Fragment toolsFragment = new ToolsFragment();
+        Fragment shareFragment = new ShareFragment();
+        Fragment sendFragment = new SendFragment();
+        fragmentArrayList.add(homeFragment);
+        fragmentArrayList.add(galleryFragment);
+        fragmentArrayList.add(slideshowFragment);
+        fragmentArrayList.add(toolsFragment);
+        fragmentArrayList.add(shareFragment);
+        fragmentArrayList.add(sendFragment);
+    }*/
+}
---
+++ app/src/main/java/military/discount/info/ui/home/HomeFragment.java
@@ -0,0 +1,69 @@
+package military.discount.info.ui.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import military.discount.info.R;
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+
+    private HomeViewModel homeViewModel;
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private EditText searchBar;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        FragmentManager fm = getChildFragmentManager();
+        mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        searchBar = (EditText)root.findViewById(R.id.location_search);
+        Log.d("헤이", String.valueOf(searchBar.getText()));
+        searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    Log.d("헤이", (String) textView.getText());
+                }
+                return true;
+            }
+        });
+        return root;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng center = new LatLng(37.504198, 126.956875);
+        mMap.addMarker(new MarkerOptions().position(center).title("중앙대"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(center));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+}
