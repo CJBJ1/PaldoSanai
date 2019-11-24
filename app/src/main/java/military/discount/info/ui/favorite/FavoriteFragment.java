7d66f54eb92b4c38c2f1e9fbc69feefc7c820b29
CJBJ1 <cbjx17@naver.com>
2019-11-24 15:46:47 +0900

    RecyclerView 데코레이션 관련 클래스 추가

--- app/src/main/java/military/discount/info/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/military/discount/info/ui/favorite/FavoriteFragment.java
@@ -13,9 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import military.discount.info.DividerItemDecoration;
 import military.discount.info.R;
+import military.discount.info.RecyclerDecoration;
 import military.discount.info.SimpleTextAdapter;
 
 public class FavoriteFragment extends Fragment {
@@ -37,6 +40,10 @@ public class FavoriteFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext())) ;
         // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
         SimpleTextAdapter adapter = new SimpleTextAdapter(list) ;
+        //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),R.drawable.line_divider));
+        recyclerView.addItemDecoration(new RecyclerDecoration(20));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter) ;
 
         return root;
